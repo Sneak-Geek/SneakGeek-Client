2e92c3cf0ee4c96703b65414845780566df20a45
Hoang Pham <huyhoang.pham95@gmail.com>
2020-02-22 23:41:21 -0800

    Initial attempt to add landing page.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Menu, Image, Header } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component<{}> {
+  public render(): JSX.Element {
+    return (
+      <div>
+        <Menu size={"massive"}>
+          <Menu.Menu position={"left"}>
+            <Menu.Item>
+              <img src="/snkg.png" />
+            </Menu.Item>
+            <Menu.Item link>
+              <Header as={"h3"}>News</Header>
+            </Menu.Item>
+            <Menu.Item link>
+              <Header as={"h3"}>App</Header>
+            </Menu.Item>
+            <Menu.Item link>
+              <Header as={"h3"}>Help</Header>
+            </Menu.Item>
+          </Menu.Menu>
+
+          <Menu.Menu position={"right"}>
+            <Menu.Item link>
+              <Header as={"h3"}>Search</Header>
+            </Menu.Item>
+            <Menu.Item link>
+              <Header as={"h3"}>Login</Header>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <div className={"ui footer form-page"}>
+          <Menu inverted secondary>
+            <Menu.Item href={"robots.txt"}>
+              <Header as={"h5"}>Privacy policies</Header>
+            </Menu.Item>
+            <Menu.Item href={"robots.txt"}>
+              <Header as={"h5"}>Terms of use</Header>
+            </Menu.Item>
+            <Menu.Item href={"robots.txt"}>
+              <Header as={"h5"}>Support</Header>
+            </Menu.Item>
+          </Menu>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
