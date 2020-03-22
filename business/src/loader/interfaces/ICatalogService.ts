d8d415f603b84d30db29e79f1d2e93732b7e7308
vcoz17 <ntduc97@gmail.com>
2020-03-22 14:40:29 -0700

    Make changes in business

--- business/src/loader/interfaces/ICatalogService.ts
+++ business/src/loader/interfaces/ICatalogService.ts
@@ -8,7 +8,6 @@ export interface ICatalogService {
     catalogDescription: string,
     products: string[]
   ): Promise<void>;
-  getShoes(token: string, value: any): Promise<Shoe[] | undefined>;
   saveCatalog(token: string, catalog: Catalog, catalogID: string): Promise<void>;
   getCatalogByTag(token: string, tag: string): Promise<Catalog>;
 }
