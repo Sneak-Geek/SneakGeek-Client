d1ff42fdc2ca67ef563e980d74f1f4804672dcad
vcoz17 <ntduc97@gmail.com>
2020-04-27 14:50:32 -0700

    Implement Request Product Screen

--- expo/src/resources/images.ts
+++ expo/src/resources/images.ts
@@ -1,9 +1,11 @@
 export const images = {
-  Home: require("../../assets/icons/home.png"),
-  Logo: require("../../assets/icons/LogoLight.png"),
-  Facebook: require("../../assets/icons/facebook.png"),
-  Google: require("../../assets/icons/google.png"),
-  Email: require("../../assets/icons/email.png"),
-  Profile: require("../../assets/icons/profile.jpg"),
-  ImagePlaceholder: require("../../assets/icons/ImagePlaceholder.jpg")
-}
+  Home: require('../../assets/icons/home.png'),
+  Logo: require('../../assets/icons/LogoLight.png'),
+  Facebook: require('../../assets/icons/facebook.png'),
+  Google: require('../../assets/icons/google.png'),
+  Email: require('../../assets/icons/email.png'),
+  Profile: require('../../assets/icons/profile.jpg'),
+  ImagePlaceholder: require('../../assets/icons/ImagePlaceholder.jpg'),
+  ImageNotUploaded: require('../../assets/icons/ImageNotUploaded.png'),
+  ImageNotUploadedx2: require('../../assets/icons/ImageNotUploadedx2.png'),
+};
