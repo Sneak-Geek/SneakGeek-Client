823a64f95c4d4e8c5b9847fd76703e86140ce6b9
Hoang Pham <hopha@sneakgeek.app>
2020-05-03 13:36:53 -0700

    Fix archive certificate for iOS. (#43)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -41,7 +41,7 @@ export default function App(): JSX.Element {
     Factory.register<IEnvVar>(Keys.IEnvVar, {
       dev: __DEV__,
       devUrl: 'http://10.0.0.159:8080/api/v1',
-      prodUrl: 'https://sneakgeek-test.azurewebsites.net/api/v1',
+      prodUrl: 'https://sneakgeek-dev.azurewebsites.net/api/v1',
     });
     Factory.register<IFacebookSDK>(Keys.IFacebookSDK, new FacebookSdk());
     Factory.register<IAccountService>(
