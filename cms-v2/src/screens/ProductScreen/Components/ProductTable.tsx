94766f292ff9c2761f42e5e1873281a7ef0a45ef
vcoz17 <hopha@sneakgeek.app>
2020-05-25 15:29:22 -0700

    Add search for products.

--- cms-v2/src/screens/ProductScreen/Components/ProductTable.tsx
+++ cms-v2/src/screens/ProductScreen/Components/ProductTable.tsx
@@ -34,26 +34,87 @@ const useStyles = makeStyles((theme: Theme) => ({
   title: {
     marginBottom: 15,
   },
+  total: {
+    marginLeft: theme.spacing(4),
+    marginBottom: theme.spacing(2),
+  },
 }));
+
+const ProductTableContent = (props: { shoes: Shoe[]; classes: any }): JSX.Element => (
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell />
+        <TableCell>Title</TableCell>
+        <TableCell>Brand</TableCell>
+        <TableCell>Colorway</TableCell>
+        <TableCell>Category</TableCell>
+        <TableCell>Action</TableCell>
+      </TableRow>
+    </TableHead>
+    {props.shoes.map((shoe) => (
+      <TableRow hover key={shoe._id}>
+        <TableCell>
+          <Avatar src={shoe.imageUrl} variant={'rounded'} sizes={'large'} />
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">{shoe.title}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">{shoe.brand}</Typography>
+        </TableCell>
+        <TableCell>
+          {shoe.colorway.map((c) => (
+            <Chip
+              clickable
+              label={c}
+              key={c}
+              className={props.classes.chip}
+              color={'inherit'}
+            />
+          ))}
+        </TableCell>
+        <TableCell>
+          <Chip clickable label={shoe.category} />
+        </TableCell>
+        <TableCell className={props.classes.action}>
+          <IconButton>
+            <Icon>edit</Icon>
+          </IconButton>
+          <IconButton>
+            <Icon>arrow_forward</Icon>
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    ))}
+  </Table>
+);
 
 export const ProductTable = (): JSX.Element => {
   const classes = useStyles();
   const shoeService: IShoeService = ObjectFactory.getObjectInstance(FactoryKeys.IShoeService);
-
-  const fetchShoes = async () => {
-    const { shoes, count } = await shoeService.searchShoes(getToken(), '', 0);
-    setState({ ...state, shoes, total: count });
-  };
-
   const [state, setState] = useState<{
     currentPage: number;
     shoes: Shoe[];
     total: number;
+    keyword: string;
   }>({
     shoes: [],
     currentPage: 0,
     total: 0,
+    keyword: '',
   });
+
+  const fetchShoes = async (keyword: string = state.keyword) => {
+    const { shoes, count } = await shoeService.searchShoes(getToken(), keyword, 0);
+    setState({ ...state, shoes, total: count });
+  };
+
+  const onSearch: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const text = event.target.value;
+    setState({ ...state, keyword: text });
+    fetchShoes(text);
+  };
 
   useEffect(() => {
     if (state.shoes.length === 0) {
@@ -68,61 +129,16 @@ export const ProductTable = (): JSX.Element => {
       </Typography>
       <Card>
         <CardContent className={classes.content}>
-          <SearchInput onChange={() => {}} />
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell />
-                <TableCell>Title</TableCell>
-                <TableCell>Brand</TableCell>
-                <TableCell>Colorway</TableCell>
-                <TableCell>Category</TableCell>
-                <TableCell>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            {state.shoes.map((shoe) => (
-              <TableRow hover key={shoe._id}>
-                <TableCell>
-                  <Avatar src={shoe.imageUrl} variant={'rounded'} sizes={'large'} />
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body1">{shoe.title}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body1">{shoe.brand}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body1">
-                    {shoe.colorway.map((c) => (
-                      <Chip
-                        clickable
-                        label={c}
-                        key={c}
-                        className={classes.chip}
-                        color={'inherit'}
-                      />
-                    ))}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body1">{shoe.category}</Typography>
-                </TableCell>
-                <TableCell className={classes.action}>
-                  <IconButton>
-                    <Icon>edit</Icon>
-                  </IconButton>
-                  <IconButton>
-                    <Icon>arrow_forward</Icon>
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </Table>
+          <SearchInput onChange={onSearch} />
+          <Typography variant={'body1'} className={classes.total}>
+            {state.total} kết quả
+          </Typography>
+          <ProductTableContent shoes={state.shoes} classes={classes} />
         </CardContent>
       </Card>
       <TablePagination
         component={'div'}
-        count={state.total / 10}
+        count={Math.ceil(state.total / 10)}
         page={state.currentPage}
         onChangePage={() => {}}
         rowsPerPageOptions={[10]}
