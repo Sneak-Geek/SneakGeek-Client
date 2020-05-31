0530ee47cc8e265d2de398e87b299f7b8b83a43f
vcoz17 <hopha@sneakgeek.app>
2020-05-31 13:37:57 -0700

    Add client support for new notification service.

--- business/src/payload/index.ts
+++ business/src/payload/index.ts
@@ -1,4 +1,4 @@
-import { Account, Profile, Catalog, Shoe, Review, SellOrder, BuyOrder, PopulatedSellOrder } from "../model";
+import { Account, Profile, Catalog, Shoe, Review, SellOrder, BuyOrder, PopulatedSellOrder, Notification } from "../model";
 
 export enum NetworkRequestState {
   NOT_STARTED,
@@ -32,5 +32,6 @@ export type HomePageCatalogsPayload = NetworkPayload<{
   toppick: Catalog;
   buynow: Catalog;
 }>;
+export type NotificationsPayload = NetworkPayload<{ notifications: Notification[] }>;
 export type GetSellOrderHistoryPayload = NetworkPayload<PopulatedSellOrder[]>;
 export type GetBuyOrdersPayload = NetworkPayload<BuyOrder[]>;
